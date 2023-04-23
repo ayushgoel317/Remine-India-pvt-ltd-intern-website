58cf1e6f918772351e584e99aa025938e04f3c7b
Ayush goel <82891154+ayushgoel317@users.noreply.github.com>
2023-04-23 14:17:56 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,5 @@
+const contactBox = document.querySelector('.contact-box');
+const nameInput = document.querySelector('input[type="text"]');
+const emailInput = document.querySelector('input[type="email"]');
+const messageTextarea = document.querySelector('textarea');
+
